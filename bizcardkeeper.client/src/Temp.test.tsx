c8286c438b2ce4f1644f5d5cff81078bca170259
KM5075 <koji19991999@gmail.com>
2025-01-30 06:25:23 +0900

    Add : Add Jest

---
+++ bizcardkeeper.client/src/Temp.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Temp } from "./Temp";
+
+describe("Temp component", () => {
+    test("renders Temp component", () => {
+        render(<Temp />);
+        expect(screen.getByText("Temp")).toBeInTheDocument();
+    });
+});
