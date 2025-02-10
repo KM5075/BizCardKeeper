b0a45d291b2fb4098b0f56251798a19208b52513
KM5075 <koji19991999@gmail.com>
2025-02-10 23:51:37 +0900

    Add : Basic additions to MSTest

--- BizCardKeeper.Server.Tests/Controllers/TempControllerTests.cs
+++ BizCardKeeper.Server.Tests/Controllers/TempControllerTests.cs
@@ -7,12 +7,34 @@ using System.Text;
 using System.Threading.Tasks;
 using BizCardKeeper.Server.Data;
 using Microsoft.EntityFrameworkCore;
+[assembly: ClassCleanupExecution(ClassCleanupBehavior.EndOfClass)]
 
 namespace BizCardKeeper.Server.Controllers.Tests
 {
     [TestClass()]
     public class TempControllerTests
     {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+
+        }
+
         [TestMethod()]
         public void GetTest()
         {
@@ -24,6 +46,36 @@ namespace BizCardKeeper.Server.Controllers.Tests
             var result = controller.Get();
             var actual = result.Result as Microsoft.AspNetCore.Mvc.OkObjectResult;
             Assert.AreEqual("Hello from TempController", actual.Value);
+        }
+
+        [TestMethod()]
+        public void PostTest()
+        {
+            var options = new DbContextOptionsBuilder<BizCardKeeperDbContext>()
+                .UseInMemoryDatabase(databaseName: "TempControllerTests")
+                .Options;
+            var context = new BizCardKeeperDbContext(options);
+            var controller = new TempController(context);
+            var temp = new BizCardKeeper.Server.Models.Temp { Id = 0, Text = "Test" };
+            var result = controller.Post(temp);
+            var actual = result.Result as Microsoft.AspNetCore.Mvc.OkObjectResult;
+            Assert.AreEqual("Temp added", actual.Value);
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            var options = new DbContextOptionsBuilder<BizCardKeeperDbContext>()
+                .UseInMemoryDatabase(databaseName: "TempControllerTests")
+                .Options;
+            var context = new BizCardKeeperDbContext(options);
+            var controller = new TempController(context);
+            var temp = new BizCardKeeper.Server.Models.Temp { Id = 0, Text = "Test" };
+            controller.Post(temp);
+            var result = controller.GetAll();
+            var actual = result.Result as Microsoft.AspNetCore.Mvc.OkObjectResult;
+            var actualList = actual.Value as List<BizCardKeeper.Server.Models.Temp>;
+            Assert.AreEqual(1, actualList.Count);
         }
     }
 }
