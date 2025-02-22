1b875944dcd74c57e994d4ceb341e01523debdc3
KM5075 <koji19991999@gmail.com>
2025-02-22 14:55:36 +0900

    Refactor : refactoring code

--- BizCardKeeper.Server/Models/User.cs
+++ BizCardKeeper.Server/Models/User.cs
@@ -4,25 +4,23 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BizCardKeeper.Server.Models
+namespace BizCardKeeper.Server.Models;
+public class User
 {
-    public class User
-    {
-        [Required]
-        public int Id { get; set; }
+    [Required]
+    public int Id { get; set; }
 
-        [Required]
-        public string Username { get; set; } = string.Empty;
+    [Required]
+    public string Username { get; set; } = string.Empty;
 
-        [Required]
-        public string Description { get; set; } = string.Empty;
+    [Required]
+    public string Description { get; set; } = string.Empty;
 
-        public string? GithubId { get; set; }
+    public string? GithubId { get; set; }
 
-        public string? QiitaId { get; set; }
+    public string? QiitaId { get; set; }
 
-        public string? TwitterId { get; set; }
+    public string? TwitterId { get; set; }
 
-        public List<Skill> Skills { get; } = [];
-    }
+    public List<Skill> Skills { get; } = [];
 }
