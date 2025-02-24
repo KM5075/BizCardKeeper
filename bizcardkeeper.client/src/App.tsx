571101b9db906192b79a28e6a913376f917cce1d
KM5075 <koji19991999@gmail.com>
2025-02-24 12:12:01 +0900

    refactor: refactor routing to App component and add Home page layout

--- bizcardkeeper.client/src/App.tsx
+++ bizcardkeeper.client/src/App.tsx
@@ -1,11 +1,33 @@
 import "./App.css";
-import { Button, Heading } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Temp } from "./Temp.tsx";
+import { Page404 } from "./components/pages/Page404.tsx";
+import { BizCard } from "./components/pages/BizCard.tsx";
+import { Home } from "./components/pages/Home.tsx";
 
 function App() {
   return (
     <div>
-      <Heading as={"h1"}>BizCartKeeper</Heading>
-      <Button bg={"teal"}>Button</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/temp"
+            element={<Temp />}
+          />
+          <Route
+            path="/cards/:id"
+            element={<BizCard />}
+          />
+          <Route
+            path="*"
+            element={<Page404 />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- bizcardkeeper.client/src/main.tsx
+++ bizcardkeeper.client/src/main.tsx
@@ -3,34 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Temp } from "./Temp.tsx";
-import { Page404 } from "./components/pages/Page404.tsx";
-import { BizCard } from "./components/pages/BizCard.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<App />}
-          />
-          <Route
-            path="/temp"
-            element={<Temp />}
-          />
-          <Route
-            path="/cards/:id"
-            element={<BizCard />}
-          />
-          <Route
-            path="*"
-            element={<Page404 />}
-          />
-        </Routes>
-      </BrowserRouter>
+      <App />
     </Provider>
   </StrictMode>
 );
