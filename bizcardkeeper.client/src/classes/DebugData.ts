4eaa5debe6836faaefb9fc4e7208546ae360ac10
KM5075 <koji19991999@gmail.com>
2025-02-24 12:27:27 +0900

    feat: add debug data for user and skills in DebugData.ts file

---
+++ bizcardkeeper.client/src/classes/DebugData.ts
@@ -0,0 +1,23 @@
+import { User } from "./User";
+import { Skill } from "./Skill";
+
+const debugSkills: Skill[] = [
+  new Skill(1, "React"),
+  new Skill(2, "Vue"),
+  new Skill(3, "Node.js"),
+  new Skill(4, "Express"),
+  new Skill(5, "MySQL"),
+  new Skill(6, "PostgreSQL"),
+  new Skill(7, "Docker"),
+  new Skill(8, "AWS"),
+];
+
+export const debugUser: User = new User(
+  1,
+  "John Doe",
+  "Software Engineer",
+  "johndoe",
+  "johndoe",
+  "johndoe",
+  debugSkills
+);
