6ce8ce5751e9b131fea17bb0d0be98e9cfc53a8a
KM5075 <koji19991999@gmail.com>
2025-03-02 14:01:22 +0900

    feat: fetch user data with a delay and handle loading state

--- bizcardkeeper.client/src/components/pages/BizCard.tsx
+++ bizcardkeeper.client/src/components/pages/BizCard.tsx
@@ -2,13 +2,12 @@ import { Heading, Text } from "@chakra-ui/react";
 import { useParams } from "react-router";
 import { PrimaryButton } from "../atoms/PrimaryButton";
 import { User } from "../../classes/User";
-import { debugUser } from "../../classes/DebugData";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const BizCard = () => {
   const { id } = useParams<{ id: string }>();
-  const [user, setUser] = useState<User>(debugUser);
+  const [user, setUser] = useState<User>();
 
   const featchUser = async () => {
     await axios.get<User>("/api/cards/" + id).then((res) => {
@@ -16,6 +15,10 @@ export const BizCard = () => {
       setUser(res.data);
     });
   }
+
+  useEffect(() => {
+    setTimeout(() => featchUser(), 1000);
+  }, []);
 
   return (
     <div>
@@ -25,13 +28,19 @@ export const BizCard = () => {
         label="Load"
         onClick={featchUser}
       />
-      <Text>ID : {id}</Text>
-      <Text>名前 : {user.name}</Text>
-      <Text>自己紹介 : {user.description}</Text>
-      <Text>スキル:{user.skills.map((skill) => skill.name).join(",")}</Text>
-      <Text>Github : {user.github_id} </Text>
-      <Text>Qiita : {user.qiita_id} </Text>
-      <Text>X : {user.x_id} </Text>
+      {user ?
+        <div>
+          <Text>ID : {id}</Text>
+          <Text>名前 : {user.name}</Text>
+          <Text>自己紹介 : {user.description}</Text>
+          <Text>スキル:{user.skills.map((skill) => skill.name).join(",")}</Text>
+          <Text>Github : {user.github_id} </Text>
+          <Text>Qiita : {user.qiita_id} </Text>
+          <Text>X : {user.x_id} </Text>
+        </div>
+        :
+        <Text>データがありません</Text>
+      }
     </div>
   );
 };
