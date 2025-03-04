4fa8965f737959ea4d5e150722ff71ecd42b61d0
KM5075 <koji19991999@gmail.com>
2025-03-04 20:17:23 +0900

    feat: improve BizCard component layout and enhance user information display

--- bizcardkeeper.client/src/components/pages/BizCard.tsx
+++ bizcardkeeper.client/src/components/pages/BizCard.tsx
@@ -41,9 +41,37 @@ export const BizCard = () => {
             mb={4}>
             {user.userName}
           </Heading>
-          <Text>ID : {id}</Text>
-          <Text>自己紹介 : {user.description}</Text>
-          <Text>スキル:{user.skills.map((skill) => skill.name).join(",")}</Text>
+          <Text fontWeight={"bold"}>ID</Text>
+          <Text>{user.id}</Text>
+          <br />
+          <Text fontWeight={"bold"}>自己紹介</Text>
+          <Text>{user.description}</Text>
+          <br />
+          <Text fontWeight={"bold"}>好きな技術</Text>
+          <Text>{user.skills.map((skill) => skill.name).join(",")}</Text>
+          <br />
+          <Box
+            display={"flex"}
+            alignItems={"center"}>
+            <Box
+              as="a"
+              href={`https://github.com/${user.githubId}`}
+              target="_blank"
+              color="black">
+              <svg
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                width="24px"
+                height="24px"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.49.5.09.68-.22.68-.48 0-.24-.01-.87-.01-1.71-2.78.61-3.37-1.34-3.37-1.34-.45-1.15-1.11-1.46-1.11-1.46-.91-.62.07-.61.07-.61 1.01.07 1.54 1.04 1.54 1.04.9 1.54 2.36 1.1 2.94.84.09-.65.35-1.1.63-1.35-2.22-.25-4.56-1.11-4.56-4.95 0-1.09.39-1.98 1.03-2.68-.1-.25-.45-1.27.1-2.65 0 0 .84-.27 2.75 1.02A9.56 9.56 0 0112 6.8c.85.004 1.71.115 2.51.337 1.91-1.29 2.75-1.02 2.75-1.02.55 1.38.2 2.4.1 2.65.64.7 1.03 1.59 1.03 2.68 0 3.85-2.34 4.7-4.57 4.95.36.31.68.92.68 1.85 0 1.34-.01 2.42-.01 2.75 0 .27.18.58.69.48A10.003 10.003 0 0022 12c0-5.52-4.48-10-10-10z"
+                />
+              </svg>
+            </Box>
+          </Box>
           <Text>Github : {user.githubId} </Text>
           <Text>Qiita : {user.qiitaId} </Text>
           <Text>X : {user.twitterId} </Text>
