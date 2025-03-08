1904a24ed89da6f459ba94c473ce2e19f506104a
KM5075 <koji19991999@gmail.com>
2025-03-08 13:23:13 +0900

    feat: integrate useLoginUser hook to manage login user state in useAuth

--- bizcardkeeper.client/src/hooks/useAuth.ts
+++ bizcardkeeper.client/src/hooks/useAuth.ts
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useLoginUser } from "./useLoginUser";
 
 export const useAuth = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
+  const { setLoginUser } = useLoginUser();
   const login = (userId: string) => {
     setLoading(true);
+    setLoginUser({ id: userId, isAdmin: userId === "admin" });
     console.log(`login: ${userId}`);
     navigate("/home");
     setLoading(false);
