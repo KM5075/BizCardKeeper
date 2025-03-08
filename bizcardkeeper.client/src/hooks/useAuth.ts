780985997111aa92f414098fd7d0f04f64d46bfe
KM5075 <koji19991999@gmail.com>
2025-03-08 13:13:49 +0900

    feat: add loading state to useAuth hook during login process

--- bizcardkeeper.client/src/hooks/useAuth.ts
+++ bizcardkeeper.client/src/hooks/useAuth.ts
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const useAuth = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const login = (userId: string) => {
+    setLoading(true);
     console.log(`login: ${userId}`);
     navigate("/home");
+    setLoading(false);
   };
 
   return {
     login,
+    loading,
   };
 };
