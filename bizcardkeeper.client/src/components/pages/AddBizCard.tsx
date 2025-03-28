d391c27b94f8c003487c82af5e5b4ecd1fa618d2
KM5075 <koji19991999@gmail.com>
2025-03-29 08:22:11 +0900

    feat: enhance AddBizCard component with additional input fields for user information

--- bizcardkeeper.client/src/components/pages/AddBizCard.tsx
+++ bizcardkeeper.client/src/components/pages/AddBizCard.tsx
@@ -1,8 +1,67 @@
+import { Box, Heading, Input, Text } from "@chakra-ui/react";
+
 export const AddBizCard = () => {
   return (
-    <div>
-      <h1>Add Card</h1>
-      <p>This is the Add Card page.</p>
-    </div>
+    <Box
+      bg={"white"}
+      p={4}
+      borderRadius="md"
+      boxShadow="md">
+      <Heading
+        size="3xl"
+        mb={4}>
+        New BizCard
+      </Heading>
+      <Box>
+        <Text
+          fontSize="lg"
+          textAlign={"left"}
+          mb={2}>
+          お名前 *
+        </Text>
+
+        <Input mb={4} />
+
+        <Text
+          fontSize="lg"
+          textAlign={"left"}
+          mb={2}>
+          自己紹介 *
+        </Text>
+        <Input mb={4} />
+
+        <Text
+          fontSize="lg"
+          textAlign={"left"}
+          mb={2}>
+          好きな技術 *
+        </Text>
+        <Input mb={4} />
+
+        <Text
+          fontSize="lg"
+          textAlign={"left"}
+          mb={2}>
+          Github ID
+        </Text>
+        <Input mb={4} />
+
+        <Text
+          fontSize="lg"
+          textAlign={"left"}
+          mb={2}>
+          Qiita ID
+        </Text>
+        <Input mb={4} />
+
+        <Text
+          fontSize="lg"
+          textAlign={"left"}
+          mb={2}>
+          Twitter ID
+        </Text>
+        <Input mb={4} />
+      </Box>
+    </Box>
   );
 };
