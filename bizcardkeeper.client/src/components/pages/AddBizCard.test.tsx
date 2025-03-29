24d2f598c558c37aea7afe66207d31cbd29a7fbc
KM5075 <koji19991999@gmail.com>
2025-03-29 13:40:14 +0900

    feat: implement form handling in AddBizCard component with validation and submit button

--- bizcardkeeper.client/src/components/pages/AddBizCard.test.tsx
+++ bizcardkeeper.client/src/components/pages/AddBizCard.test.tsx
@@ -1,7 +1,25 @@
 import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { AddBizCard } from "./AddBizCard";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import userEvent from "@testing-library/user-event";
 
 describe("AddBizCard", () => {
   it("template test", () => {
     expect(true).toBe(true);
+  });
+  it("should render some validation", async () => {
+    render(
+      <ChakraProvider value={defaultSystem}>
+        <AddBizCard />
+      </ChakraProvider>
+    );
+
+    const registrarButton = screen.getByTestId("submit-button");
+
+    userEvent.click(registrarButton);
+
+    expect(await screen.findByText("ユーザ名は必須です。")).toBeInTheDocument();
+    expect(await screen.findByText("自己紹介は必須です。")).toBeInTheDocument();
   });
 });
--- bizcardkeeper.client/src/components/pages/AddBizCard.tsx
+++ bizcardkeeper.client/src/components/pages/AddBizCard.tsx
@@ -1,6 +1,36 @@
 import { Box, Heading, Input, Text } from "@chakra-ui/react";
+import { PrimaryButton } from "../atoms/PrimaryButton";
+import { useForm } from "react-hook-form";
+import { User } from "../../classes/User";
+import { Field } from "../ui/field";
+import { SubmitButton } from "../atoms/SubmitButton";
 
 export const AddBizCard = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>({
+    defaultValues: {
+      id: 0,
+      userName: "",
+      description: "",
+      skills: [],
+      githubId: "",
+      qiitaId: "",
+      twitterId: "",
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+
+    // ここでAPIにデータを送信する処理を追加します。
+    // 例: axios.post('/api/bizcard', data)
+    // .then(response => console.log(response))
+    // .catch(error => console.error(error));
+  });
+
   return (
     <Box
       bg={"white"}
@@ -13,54 +43,86 @@ export const AddBizCard = () => {
         New BizCard
       </Heading>
       <Box>
-        <Text
-          fontSize="lg"
-          textAlign={"left"}
-          mb={2}>
-          お名前 *
-        </Text>
+        <form onSubmit={onSubmit}>
+          <Text
+            fontSize="lg"
+            textAlign={"left"}
+            mb={2}>
+            お名前 *
+          </Text>
+          <Field
+            label="ユーザ名"
+            invalid={!!errors.userName}
+            errorText={errors.userName?.message}>
+            <Input
+              mb={4}
+              {...register("userName", {
+                required: "ユーザ名は必須です。",
+                maxLength: {
+                  value: 20,
+                  message: "20文字以内で入力してください。",
+                },
+              })}
+            />
+          </Field>
 
-        <Input mb={4} />
+          <Field
+            label="自己紹介"
+            invalid={!!errors.description}
+            errorText={errors.description?.message}>
+            <Input
+              mb={4}
+              {...register("description", {
+                required: "自己紹介は必須です。",
+                maxLength: {
+                  value: 100,
+                  message: "100文字以内で入力してください。",
+                },
+              })}
+            />
+          </Field>
 
-        <Text
-          fontSize="lg"
-          textAlign={"left"}
-          mb={2}>
-          自己紹介 *
-        </Text>
-        <Input mb={4} />
+          <Text
+            fontSize="lg"
+            textAlign={"left"}
+            mb={2}>
+            自己紹介 *
+          </Text>
 
-        <Text
-          fontSize="lg"
-          textAlign={"left"}
-          mb={2}>
-          好きな技術 *
-        </Text>
-        <Input mb={4} />
+          <Text
+            fontSize="lg"
+            textAlign={"left"}
+            mb={2}>
+            好きな技術 *
+          </Text>
+          <Input mb={4} />
 
-        <Text
-          fontSize="lg"
-          textAlign={"left"}
-          mb={2}>
-          Github ID
-        </Text>
-        <Input mb={4} />
+          <Text
+            fontSize="lg"
+            textAlign={"left"}
+            mb={2}>
+            Github ID
+          </Text>
+          <Input mb={4} />
 
-        <Text
-          fontSize="lg"
-          textAlign={"left"}
-          mb={2}>
-          Qiita ID
-        </Text>
-        <Input mb={4} />
+          <Text
+            fontSize="lg"
+            textAlign={"left"}
+            mb={2}>
+            Qiita ID
+          </Text>
+          <Input mb={4} />
 
-        <Text
-          fontSize="lg"
-          textAlign={"left"}
-          mb={2}>
-          Twitter ID
-        </Text>
-        <Input mb={4} />
+          <Text
+            fontSize="lg"
+            textAlign={"left"}
+            mb={2}>
+            Twitter ID
+          </Text>
+          <Input mb={4} />
+
+          <SubmitButton>登録</SubmitButton>
+        </form>
       </Box>
     </Box>
   );
