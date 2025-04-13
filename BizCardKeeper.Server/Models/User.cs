2158a7e904756b22a4aa3b0e826b2563e046fc4a
KM5075 <koji19991999@gmail.com>
2025-04-13 11:52:38 +0900

    docs: enhance XML documentation for User model properties

--- BizCardKeeper.Server/Models/User.cs
+++ BizCardKeeper.Server/Models/User.cs
@@ -6,23 +6,47 @@ using System.Threading.Tasks;
 
 namespace BizCardKeeper.Server.Models;
 
+/// <summary>
+/// Represents a user in the system.
+/// </summary>
 public class User
 {
+    /// <summary>
+    /// Gets or sets the unique identifier for the user.
+    /// </summary>
     [Required]
     public int Id { get; set; }
 
+    /// <summary>
+    /// Gets or sets the username of the user.
+    /// </summary>
     [Required]
     public string UserName { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Gets or sets the description of the user.
+    /// </summary>
     [Required]
     public string Description { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Gets or sets the GitHub ID of the user.
+    /// </summary>
     public string? GithubId { get; set; }
 
+    /// <summary>
+    /// Gets or sets the Qiita ID of the user.
+    /// </summary>
     public string? QiitaId { get; set; }
 
+    /// <summary>
+    /// Gets or sets the Twitter ID of the user.
+    /// </summary>
     public string? TwitterId { get; set; }
 
+    /// <summary>
+    /// Gets or sets the list of skills associated with the user.
+    /// </summary>
     public List<Skill> Skills { get; set; } = [];
 }
 
