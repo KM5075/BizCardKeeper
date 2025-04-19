ebced9d8e7afa84aa1a3d18b45ab603f738fc7fb
KM5075 <koji19991999@gmail.com>
2025-04-19 14:28:09 +0900

    refactor: remove Temp model, controller, and related tests to streamline the codebase

--- BizCardKeeper.Server/Migrations/20250419052738_delete-temp-table.Designer.cs
+++ BizCardKeeper.Server/Migrations/20250419052738_delete-temp-table.Designer.cs
@@ -3,6 +3,7 @@ using BizCardKeeper.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BizCardKeeper.Server.Migrations
 {
     [DbContext(typeof(BizCardKeeperDbContext))]
-    partial class BizCardKeeperDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250419052738_delete-temp-table")]
+    partial class deletetemptable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
