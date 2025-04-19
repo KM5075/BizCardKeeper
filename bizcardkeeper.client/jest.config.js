046e2222b9e05fe29c0ee9980bc630583da9f47b
KM5075 <koji19991999@gmail.com>
2025-04-19 12:00:57 +0900

    feat: update Jest configuration and add new Playwright tests

--- bizcardkeeper.client/jest.config.js
+++ bizcardkeeper.client/jest.config.js
@@ -1,6 +1,8 @@
 export default {
     preset: 'ts-jest',
     testEnvironment: 'jest-fixed-jsdom',
+    testMatch: ['<rootDir>/src/**/*.(test).ts?(x)'], // フロントのみに限定
+    testPathIgnorePatterns: ['/node_modules/', '<rootDir>/e2e/'], // Playwrightのパスを除外
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
